6b9e5ad59e5efad50f6af90532b4997a8bb408ca
JASeveriche <juliabytw@gmail.com>
2023-04-04 23:01:39 -0300

    AlmostDoneFirstPart

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,48 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace player.mov
-{
+
 
     public class Player : MonoBehaviour
+
     {
-        Rigidbody rigid;
-        [SerializeField] float speed =200;
+            public float moveSpeed = 5f;
+    public float jumpForce = 2f;
+    private bool isGrounded;
 
-        private void Awake()
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Cambio de los valores de entrada de movimiento a WASD
+        if (Input.GetKey(KeyCode.A))
         {
-            rigid = GetComponent<Rigidbody>();
+            horizontal = -1f;
         }
-
-
-        private void FixedUpdate()
+        if (Input.GetKey(KeyCode.D))
         {
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
-            rigid.velocity = move*speed*Time.deltaTime; // normalizador frames
+            horizontal = 1f;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            vertical = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            vertical = -1f;
         }
 
-        //min 30
-         public Vector2 MoveInput(){
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
 
-            float x= Input.GetAxis["Horizontal"];
-            float y= Input.GetAxis["Vertical"];
 
-            return new Vector2(x, y);
 
-        }
-        
-        public Vector2 MouseInput(){
-
-            float x= Input.GetAxis["Mouse X"];
-            float y= Input.GetAxis["Mouse Y"];
-
-            return new Vector2(x, y);
-
+        // Cambio de la tecla de salto a espacio
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
+        
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                isGrounded = true;
+            }
+        }
+        public Vector2 MouseInput(){
 
+            float x= Input.GetAxis("Mouse X");
+            float y= Input.GetAxis("Mouse Y");
 
+            return new Vector2(x, y);
 
+    }
 }
+    
+
+
+
+
 
 
